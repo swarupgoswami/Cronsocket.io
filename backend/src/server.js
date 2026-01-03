ab9846d2c01b2e55659cc25831690cf9bdeb2721
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-03 19:43:19 +0530

    added the cors into the server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,38 @@
 import express from "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 
 
 const app=express();
 
+const __dirname=path.resolve();
+
+app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
 
-app.get('/',(req,res)=>{
-    console.log("...")
+
+app.get('/health',(req,res)=>{
+    // console.log("...")
+    res.status(200).json({message:"api is running"});
 });
+
+
+app.get('/books',(req,res)=>{
+    // console.log("...")
+    res.status(200).json({message:"api is running"});
+});
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 
 app.listen(ENV.PORT,()=>{console.log("server is running on localhost3000");
