2ad19dff358e315ee96d51f8fd9b78d5c1c4ceb5
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-06 18:51:17 +0530

    added the script package

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import path from "path";
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import {serve} from "inngest/express";
+import { inngest } from "./lib/inngest.js";
 
 
 
@@ -12,6 +14,9 @@ const __dirname=path.resolve();
 
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+
+app.use("/api/inngest", serve({client:inngest, functions}))
 
 
 
