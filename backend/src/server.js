40e6cd19ff1ac7bd46f89c19f930d5233c089737
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-07 00:00:28 +0530

    updated the server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,45 +1,49 @@
 import express from "express";
 import path from "path";
-import {ENV} from "./lib/env.js";
+import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import {serve} from "inngest/express";
-import { inngest } from "./lib/inngest.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+import clerkWebhook from "./routes/clerkWebhook.js";
 
+const app = express();
 
-
-const app=express();
-
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+    dev: true,
+  })
+);
 
-app.use("/api/inngest", serve({client:inngest, functions}))
+app.use("/api/clerk", clerkWebhook);
 
-
-
-app.get('/health',(req,res)=>{
-    // console.log("...")
-    res.status(200).json({message:"api is running"});
+app.get("/health", (req, res) => {
+  // console.log("...")
+  res.status(200).json({ message: "api is running" });
 });
 
-
-app.get('/books',(req,res)=>{
-    // console.log("...")
-    res.status(200).json({message:"api is running"});
+app.get("/books", (req, res) => {
+  // console.log("...")
+  res.status(200).json({ message: "api is running" });
 });
 
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-app.listen(ENV.PORT,()=>{console.log("server is running on localhost3000");
-    connectDB();
+app.listen(ENV.PORT, () => {
+  console.log("server is running on localhost3000");
+  connectDB();
 });
