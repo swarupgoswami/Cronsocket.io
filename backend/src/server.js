60586c35d2b4cee5db746cb6d4d2a0d7c696904a
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-08 20:58:46 +0530

    updated server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,12 @@ import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
 import clerkWebhook from "./routes/clerkWebhook.js";
+import { clerkMiddleware } from '@clerk/express'
+import { protectroute } from "./middlewares/protectRoute.js";
+import chatRoutes from "./routes/chatRoutes.js";
+
+
+
 
 const app = express();
 
@@ -13,6 +19,10 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+
+
+app.use(clerkMiddleware())
 
 app.use(
   "/api/inngest",
@@ -22,6 +32,10 @@ app.use(
     dev: true,
   })
 );
+app.use("/api/chat",chatRoutes);
+
+
+
 
 app.use("/api/clerk", clerkWebhook);
 
@@ -30,10 +44,9 @@ app.get("/health", (req, res) => {
   res.status(200).json({ message: "api is running" });
 });
 
-app.get("/books", (req, res) => {
-  // console.log("...")
-  res.status(200).json({ message: "api is running" });
-});
+app.get('/video-calls',protectroute,(req,res)=>{
+  res.status(200).json({message:"this is a protected route"});
+})
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
