3d3c7fd2663f9507612456371dc15d80caedfb12
swarup Goswami <goswamiswarup97@gmail.com>
2026-01-16 18:58:25 +0530

    fixed the middleware

--- backend/src/middlewares/protectRoute.js
+++ backend/src/middlewares/protectRoute.js
@@ -1,32 +1,56 @@
-import { requireAuth } from '@clerk/express';
-import User from '../models/User.js';
+import { requireAuth } from "@clerk/express";
+import User from "../models/User.js";
 
+export const protectroute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
 
-export const protectroute=[
-    requireAuth(),
-    async(req,res,next)=>{
-        try {
-            const clerkId=req.auth().userId;
+      if (!clerkId) {
+        return res.status(401).json({ msg: "unauthorized - invalid token" });
+      }
 
-            if(!clerkId){
-                return res.status(401).json
-                ({msg:"unauthorized - invalid token"});
+      const user = await User.findOne({ clerkId });
 
+      if (!user) {
+        // return res.status(404).json({message:"user not found"});
+        // user = await User.create({
+        //   clerkId,
+        //   email: req.auth().sessionClaims?.email,
+        //   name:
+        //     req.auth().sessionClaims?.name ||
+        //     `${req.auth().sessionClaims?.firstName || ""} ${
+        //       req.auth().sessionClaims?.lastName || ""
+        //     }`,
+        //   profileImage: req.auth().sessionClaims?.picture,
+        // });
+        const claims = req.auth().sessionClaims;
 
-            }
+        user = await User.create({
+          clerkId,
+          email:
+            claims?.email ||
+            claims?.email_address ||
+            `${clerkId}@placeholder.local`, // fallback to avoid crash
 
-            const user=await User.findOne({clerkId});
+          name:
+            claims?.name ||
+            `${claims?.firstName || ""} ${claims?.lastName || ""}`.trim() ||
+            "New User",
 
-            if(!user){
-                return res.status(404).json({message:"user not found"});
-            }
+          profileImage: claims?.picture || null,
+        });
+        console.log("CLERK CLAIMS:", req.auth().sessionClaims);
 
-            req.user=user;
+      }
 
-            next();
-        } catch (error) {
-            console.error("error in protect route middleware", error);
-            res.status(500).json({message:"{internal server error"});
-        }
+      req.user = user;
+
+      next();
+    } catch (error) {
+      console.error("error in protect route middleware", error);
+      res.status(500).json({ message: "{internal server error" });
     }
-]
+  },
+];
